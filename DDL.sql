14a62c3810f14414e8ecade772385c7ee7a848de
Emanuel Mineda Carneiro <mineda@gmail.com>
2020-08-21 16:04:12 -0300

    Commit inicial

---
+++ DDL.sql
@@ -0,0 +1,30 @@
+create schema anotacao;
+
+use anotacao;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on anotacao.* to user@'localhost';
+
+create table usr_usuario (
+  usr_id bigint unsigned not null auto_increment,
+  usr_nome varchar(20) not null,
+  usr_senha varchar(50) not null,
+  primary key (usr_id),
+  unique key uni_usuario_nome (usr_nome)
+);
+
+create table aut_autorizacao (
+  aut_id bigint unsigned not null auto_increment,
+  aut_nome varchar(20) not null,
+  primary key (aut_id),
+  unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_autorizacao (
+  usr_id bigint unsigned not null,
+  aut_id bigint unsigned not null,
+  primary key (usr_id, aut_id),
+  foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
+  foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
+);
---
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
