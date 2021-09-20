60863f1638977aad57fd9abc9eab64c81afbd69b
Emanuel Mineda Carneiro <mineda@gmail.com>
2021-09-20 08:51:35 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -9,7 +9,7 @@ grant select, insert, delete, update on anotacao.* to user@'localhost';
 create table usr_usuario (
   usr_id bigint unsigned not null auto_increment,
   usr_nome varchar(20) not null,
-  usr_senha varchar(50) not null,
+  usr_senha varchar(100) not null,
   primary key (usr_id),
   unique key uni_usuario_nome (usr_nome)
 );
@@ -28,3 +28,9 @@ create table uau_usuario_autorizacao (
   foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
   foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
 );
+
+insert into usr_usuario (usr_nome, usr_senha)
+    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+insert into aut_autorizacao (aut_nome)
+    values ('ROLE_ADMIN');
+insert into uau_usuario_autorizacao values (1, 1);
